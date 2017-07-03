83c8c4bc475375557c0d6714a666ff7d022402f8
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-07-03 19:53:20 +0200

    Collection can have predifined values by constrcutor

--- src/Collection.php
+++ src/Collection.php
@@ -6,4 +6,13 @@ namespace Phizzl\Deployee;
 class Collection implements CollectionInterface
 {
     use IteratorImplementation;
+
+    /**
+     * Collection constructor.
+     * @param array $array
+     */
+    public function __construct(array $array = [])
+    {
+        $this->array = $array;
+    }
 }

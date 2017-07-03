47c1dda0ba5b41baefdd53959f4910a262ab289f
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-07-03 22:08:36 +0200

    Added example deployment file

---
+++ deployments/DeployDefinition_1499068619_testDirectoryTasks.php
@@ -0,0 +1,37 @@
+<?php
+
+class DeployDefinition_1499068619_testDirectoryTasks extends \Phizzl\Deployee\Plugins\Deploy\Definitions\AbstractDeploymentDefinition
+{
+    public function define()
+    {
+        $this
+            ->directory(__DIR__ . '/test/peter/oscar')
+            ->create()
+            ->recursive();
+
+        $this
+            ->file(__DIR__ . '/deploy.txt')
+            ->contents("This is an awesome content " . microtime(true));
+
+        $this
+            ->directory(__DIR__ . '/test/peter/oscar')
+            ->remove();
+
+        $this
+            ->directory(__DIR__ . '/test')
+            ->remove()
+            ->recursive();
+
+        $this
+            ->file(__DIR__ . '/deploy.txt')
+            ->remove();
+
+        $this
+            ->file(__DIR__ . '/.gittest')
+            ->copy(__DIR__ . '/.gitkeep');
+
+        $this
+            ->file(__DIR__ . '/.gittest')
+            ->remove();
+    }
+}

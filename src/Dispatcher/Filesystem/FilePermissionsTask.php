d4936e30fdcd7ee6246710db1969d2b920066e18
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-07-03 20:52:49 +0200

    Added file permission task

---
+++ src/Dispatcher/Filesystem/FilePermissionsTask.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace Phizzl\Deployee\Dispatcher\Filesystem;
+
+
+use Phizzl\Deployee\Collection;
+use Phizzl\Deployee\Tasks\TaskInterface;
+
+class FilePermissionsTask implements TaskInterface
+{
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var int
+     */
+    private $permissions;
+
+    /**
+     * @var string
+     */
+    private $owner;
+
+    /**
+     * @var string
+     */
+    private $group;
+
+    /**
+     * @var bool
+     */
+    private $recursive;
+
+    /**
+     * DirectoryTask constructor.
+     * @param string $path
+     */
+    public function __construct($path)
+    {
+        $this->path = $path;
+        $this->owner = '';
+        $this->group = '';
+        $this->permissions = 0777;
+        $this->recursive = false;
+    }
+
+    /**
+     * @param int $permissions
+     * @return $this
+     */
+    public function permissions($permissions)
+    {
+        $this->permissions = $permissions;
+        return $this;
+    }
+
+    /**
+     * @param string $owner
+     * @return $this
+     */
+    public function owner($owner)
+    {
+        $this->owner = $owner;
+        return $this;
+    }
+
+    /**
+     * @param string $group
+     * @return $this
+     */
+    public function group($group)
+    {
+        $this->group = $group;
+        return $this;
+    }
+
+    /**
+     * @param bool $recursive
+     * @return $this
+     */
+    public function recursive()
+    {
+        $this->recursive = true;
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getDefinition()
+    {
+        return new Collection([
+            'path' => $this->path,
+            'owner' => $this->owner,
+            'group' => $this->group,
+            'permissions' => $this->permissions,
+            'recursive' => $this->recursive
+        ]);
+    }
+}

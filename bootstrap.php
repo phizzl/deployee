a24531e0b8ee5e8895e4df7b6c49f760f315bc0e
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-07-10 22:09:19 +0200

    Changed finding the autoloader file in bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,24 @@
 use Phizzl\Deployee\Bootstrap\Bootstrap;
 use Phizzl\Deployee\Container;
 
-$loader = require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+$findLoader = [
+    __DIR__ . '/vendor',
+    __DIR__ . '/../../'
+];
+
+$loaderFile = '';
+foreach($findLoader as $find){
+    if(is_file($find . '/autoload.php')){
+        $loaderFile = $find . '/autoload.php';
+        break;
+    }
+}
+
+if($loaderFile === ''){
+    throw new \Exception("Could not find autoloader file");
+}
+
+$loader = require $loaderFile;
 
 $di = new Container();
 $di['composer.classloader'] = $loader;

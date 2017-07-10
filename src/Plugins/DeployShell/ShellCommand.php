a2555d84c1d12c403e554964fd2dff50aa4cc21d
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-07-10 20:59:34 +0200

    Removed unused methods from class

--- src/Plugins/DeployShell/ShellCommand.php
+++ src/Plugins/DeployShell/ShellCommand.php
@@ -42,31 +42,4 @@ class ShellCommand
             'code' => $exitCode
         ];
     }
-
-    /**
-     * @param string $command
-     * @return string
-     */
-    private function exec($command)
-    {
-
-        return trim(shell_exec($command));
-    }
-
-    /**
-     * @return int
-     */
-    private function getExitCode()
-    {
-        $exitCodeVar = $this->getOsType() === DeployShellPlugin::OS_TYPE_WIN ? "%errorlevel%" : "\$?";
-        return trim($r = $this->exec("echo $exitCodeVar")) === "" ? 255 : (int)$r;
-    }
-
-    /**
-     * @return string
-     */
-    private function getOsType()
-    {
-        return $this->container->plugins()->offsetGet(DeployShellPlugin::PLUGIN_ID)->getOsType();
-    }
 }

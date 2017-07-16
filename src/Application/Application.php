69addf151ead173e5bedc0284afe06a6b77b0c75
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-07-16 12:13:45 +0200

    Added env environment

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -5,6 +5,7 @@ namespace Deployee\Application;
 
 
 use Deployee\Container;
+use Symfony\Component\Console\Input\InputOption;
 
 class Application extends \Symfony\Component\Console\Application
 {
@@ -12,6 +13,19 @@ class Application extends \Symfony\Component\Console\Application
      * @var Container
      */
     private $container;
+
+    /**
+     * Application constructor.
+     * @param string $name
+     * @param string $version
+     */
+    public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN')
+    {
+        parent::__construct($name, $version);
+        $this->getDefinition()->addOption(
+            new InputOption('--env', null, InputOption::VALUE_OPTIONAL, "The environment to use")
+        );
+    }
 
     /**
      * @param Container $container

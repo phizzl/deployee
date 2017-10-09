83ff470bb91d81b63c2106bd688a92bc17d6fe1b
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-10-10 00:24:28 +0200

    Find shopware console after booting

--- src/Plugins/DeployShopware/Subscriber/DeployShopwareSubscriber.php
+++ src/Plugins/DeployShopware/Subscriber/DeployShopwareSubscriber.php
@@ -3,7 +3,10 @@
 namespace Deployee\Plugins\DeployShopware\Subscriber;
 
 
+use Deployee\Plugins\DeployOxid\DeployShopwarePlugin;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Deployee\Events\BootstrapFinishedEvent;
+use Deployee\Plugins\DeployShell\Services\ExecutableFinderService;
 
 class DeployShopwareSubscriber implements EventSubscriberInterface
 {
@@ -12,6 +15,39 @@ class DeployShopwareSubscriber implements EventSubscriberInterface
      */
     public static function getSubscribedEvents()
     {
-        return [];
+        return [
+            BootstrapFinishedEvent::EVENT_NAME => 'onBootstrapFinished',
+        ];
+    }
+
+    /**
+     * @param BootstrapFinishedEvent $event
+     */
+    public function onBootstrapFinished(BootstrapFinishedEvent $event)
+    {
+        $config = $event->getContainer()->plugins()->offsetGet(DeployShopwarePlugin::PLUGIN_ID)->getConfig();
+        $container = $event->getContainer();
+
+        $console = isset($config['console_path'])
+            ? $config['console_path']
+            : $this->findShopwareConsole();
+
+        $container[ExecutableFinderService::CONTAINER_ID]->addAlias("shopware", $console);
+    }
+
+    /**
+     * @return string
+     */
+    private function findShopwareConsole()
+    {
+        $paths = [getcwd(), getcwd() . DIRECTORY_SEPARATOR . 'www'];
+        foreach($paths as $path){
+            $expectedPath = $path . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'console';
+            if(is_file($expectedPath)){
+                return $expectedPath;
+            }
+        }
+
+        throw new \RuntimeException("Shopware console could not be found in: " . implode($paths));
     }
 }

8e9f281bedad14fb5b4738a7157ec4ba2d9e876a
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-10-10 00:42:37 +0200

    Added a lot of functionality to shopware subscriber

--- src/Plugins/DeployShopware/Subscriber/DeployShopwareSubscriber.php
+++ src/Plugins/DeployShopware/Subscriber/DeployShopwareSubscriber.php
@@ -7,9 +7,28 @@ use Deployee\Plugins\DeployOxid\DeployShopwarePlugin;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Deployee\Events\BootstrapFinishedEvent;
 use Deployee\Plugins\DeployShell\Services\ExecutableFinderService;
+use Deployee\Events\TaskDispatcherCollectionInitializedEvent;
+use Deployee\Plugins\Deploy\Events\TaskHelperCreatedEvent;
+use Deployee\Plugins\DeployDb\Events\ConfigureDbConnectionDataEvent;
+use Deployee\Plugins\DeployShopwareShop\ShopConfig;
+use Deployee\Plugins\DeployShopware\Dispatcher\CreateAdminTaskDispatcher;
 
 class DeployShopwareSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var array
+     */
+    private $pluginConfig;
+
+    /**
+     * DeployOxidSubscriber constructor.
+     * @param array $pluginConfig
+     */
+    public function __construct(array $pluginConfig)
+    {
+        $this->pluginConfig = $pluginConfig;
+    }
+
     /**
      * @return array
      */
@@ -17,6 +36,9 @@ class DeployShopwareSubscriber implements EventSubscriberInterface
     {
         return [
             BootstrapFinishedEvent::EVENT_NAME => 'onBootstrapFinished',
+            TaskHelperCreatedEvent::EVENT_NAME => 'onTaskHelperCreated',
+            TaskDispatcherCollectionInitializedEvent::EVENT_NAME => 'onTaskDispatcherCollectionInitialized',
+            ConfigureDbConnectionDataEvent::EVENT_NAME => 'onConfigureDbConnectionData'
         ];
     }
 
@@ -40,7 +62,12 @@ class DeployShopwareSubscriber implements EventSubscriberInterface
      */
     private function findShopwareConsole()
     {
-        $paths = [getcwd(), getcwd() . DIRECTORY_SEPARATOR . 'www'];
+        $paths = [
+            $this->pluginConfig['shop_path'],
+            getcwd() . DIRECTORY_SEPARATOR . $this->pluginConfig['shop_path'],
+            getcwd()
+        ];
+
         foreach($paths as $path){
             $expectedPath = $path . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'console';
             if(is_file($expectedPath)){
@@ -49,5 +76,67 @@ class DeployShopwareSubscriber implements EventSubscriberInterface
         }
 
         return './bin/console';
+    }
+
+    /**
+     * @param TaskHelperCreatedEvent $event
+     */
+    public function onTaskHelperCreated(TaskHelperCreatedEvent $event)
+    {
+        $taskHelper = $event->getTaskHelper();
+        $taskHelper->registerTask('Deployee\Plugins\DeployShopware\Tasks\CreateAdminTask', 'shopwareAdmin');
+    }
+
+    /**
+     * @param TaskDispatcherCollectionInitializedEvent $event
+     */
+    public function onTaskDispatcherCollectionInitialized(TaskDispatcherCollectionInitializedEvent $event)
+    {
+        $collection = $event->getTaskDispatcherCollection();
+        $collection->registerDispatcher(new CreateAdminTaskDispatcher($event->getContainer()));
+    }
+
+    /**
+     * @param ConfigureDbConnectionDataEvent $event
+     */
+    public function onConfigureDbConnectionData(ConfigureDbConnectionDataEvent $event)
+    {
+        if(isset($this->pluginConfig['configure_db_plugin'])
+            && $this->pluginConfig['configure_db_plugin'] === true){
+            $this->configureDbPlugin($event);
+        }
+    }
+
+    /**
+     * @param ConfigureDbConnectionDataEvent $event
+     */
+    private function configureDbPlugin(ConfigureDbConnectionDataEvent $event)
+    {
+        if(!isset($this->pluginConfig['shop_path'])){
+            throw new \RuntimeException("You have to configure \"shop_path\" when using \"configure_db_plugin\"");
+        }
+
+        $shopPath = "";
+        $find = [$this->pluginConfig['shop_path'], getcwd() . DIRECTORY_SEPARATOR . $this->pluginConfig['shop_path']];
+        foreach($find as $path){
+            if(realpath($path)
+                && is_file(realpath($path) . DIRECTORY_SEPARATOR . 'config.php')){
+                $shopPath = realpath($path);
+            }
+        }
+
+        if($shopPath === ""){
+            throw new \RuntimeException("Unable to locate config.php");
+        }
+
+        $dbPluginConfig = $event->getConfig();
+        $shopConfig = new ShopConfig($shopPath . DIRECTORY_SEPARATOR . "config.php");
+        $dbConfig = $shopConfig->get('db');
+        $dbPluginConfig['host'] = $dbConfig['host'];
+        $dbPluginConfig['port'] = $dbConfig['port'];
+        $dbPluginConfig['user'] = $dbConfig['username'];
+        $dbPluginConfig['password'] = $dbConfig['password'];
+        $dbPluginConfig['name'] = $dbConfig['dbname'];
+        $event->setConfig($dbPluginConfig);
     }
 }

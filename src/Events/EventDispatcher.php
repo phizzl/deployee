afbabef1c0753db138f39eaf5dbe5d712ec9e827
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-10-27 00:58:36 +0200

    Removed all event classes

--- src/Events/EventDispatcher.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Deployee\Events;
-
-
-class EventDispatcher extends \Symfony\Component\EventDispatcher\EventDispatcher
-{
-    const CONTAINER_ID = "deployee.events.dispatcher";
-}

4e50d09f58cd896e79c011447bc5948318693a3a
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-11-04 10:20:55 +0100

    Added mysql tasks to backwards compatibility

--- plugins/BackwardsCompatibility/DependencyInjectionProvider.php
+++ plugins/BackwardsCompatibility/DependencyInjectionProvider.php
@@ -4,10 +4,15 @@ namespace Deployee\Plugins\BackwardsCompatibility;
 
 
 use Deployee\Dependency\DependencyInjectionProviderInterface;
+use Deployee\Deployment\Helper\TaskCreationHelper;
+use Deployee\Deployment\Module;
 use Deployee\Kernel\Locator;
 
 class DependencyInjectionProvider implements DependencyInjectionProviderInterface
 {
+    /**
+     * @param Locator $locator
+     */
     public function injectDependencies(Locator $locator)
     {
         if(!class_exists('\Deployee\Plugins\Deploy\Definitions\AbstractDeploymentDefinition')){
@@ -16,5 +21,11 @@ class DependencyInjectionProvider implements DependencyInjectionProviderInterfac
                 '\Deployee\Plugins\Deploy\Definitions\AbstractDeploymentDefinition'
             );
         }
+
+        $locator->Dependency()->extendDependency(Module::DEFINITION_HELPER_TASK_CREATION_DEPENDENCY, function(TaskCreationHelper $helper){
+            $helper->addAlias('mysqlfile', 'Deployee\Plugins\MySqlTasks\Definitions\MySqlFileDefinition');
+            $helper->addAlias('mysqlquery', 'Deployee\Plugins\MySqlTasks\Definitions\MySqlQueryDefinition');
+            return $helper;
+        });
     }
 }

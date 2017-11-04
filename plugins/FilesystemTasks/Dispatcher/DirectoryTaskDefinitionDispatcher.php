0000ec043621d35607574d06a502b7c8fb08f876
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-11-04 07:25:50 +0100

    Added logix to directory dispatcher

--- plugins/FilesystemTasks/Dispatcher/DirectoryTaskDefinitionDispatcher.php
+++ plugins/FilesystemTasks/Dispatcher/DirectoryTaskDefinitionDispatcher.php
@@ -6,7 +6,7 @@ namespace Deployee\Plugins\FilesystemTasks\Dispatcher;
 
 use Deployee\Deployment\Definitions\Tasks\TaskDefinitionInterface;
 use Deployee\Plugins\FilesystemTasks\Definitions\DirectoryTaskDefinition;
-use Deployee\Plugins\FilesystemTasks\Utils\Rm;
+use Deployee\Plugins\FilesystemTasks\Utils\RmDir;
 use Deployee\Plugins\RunDeploy\Dispatcher\AbstractTaskDefinitionDispatcher;
 use Deployee\Plugins\RunDeploy\Dispatcher\DispatchResult;
 
@@ -27,6 +27,58 @@ class DirectoryTaskDefinitionDispatcher extends AbstractTaskDefinitionDispatcher
      */
     public function dispatch(TaskDefinitionInterface $taskDefinition)
     {
+        $definition = $taskDefinition->define();
+        if($definition->get('create') === true){
+            return $this->createDirectory($definition->get('path'), $definition->get('recursive'));
+        }
+        elseif($definition->get('remove') === true) {
+            return $this->removeDiretory($definition->get('path'), $definition->get('recursive'));
+        }
 
+        throw new \LogicException("Invalid definition");
+    }
+
+    /**
+     * @param string $directory
+     * @param bool $recursive
+     * @return DispatchResult
+     */
+    private function removeDiretory($directory, $recursive)
+    {
+        try{
+            $rmDir = new RmDir();
+            $rmDir->remove($directory, $recursive);
+        }
+        catch(\InvalidArgumentException $e){}
+        catch(\RuntimeException $e){}
+
+        return isset($e)
+            ? new DispatchResult(
+                255,
+                '',
+                sprintf("Could not remove directory %s", $directory) . PHP_EOL . print_r(error_get_last(), true)
+            )
+            : new DispatchResult(
+                0,
+                sprintf("Directory removed: %s", $directory)
+            );
+    }
+
+    /**
+     * @param string $directory
+     * @param bool $recursive
+     * @return DispatchResult
+     */
+    private function createDirectory($directory, $recursive)
+    {
+        if(!mkdir($directory, 0777, $recursive)){
+            return new DispatchResult(
+                255,
+                '',
+                sprintf("Could not create directory %s", $directory) . PHP_EOL . print_r(error_get_last(), true)
+            );
+        }
+
+        return new DispatchResult(0, sprintf("Directory created: %s", $directory));
     }
 }

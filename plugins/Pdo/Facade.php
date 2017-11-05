10a97b99fa5d47d5f9de90c40488d5f5b8c1016a
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-11-05 08:37:47 +0100

    Added methods to facade

--- plugins/Pdo/Facade.php
+++ plugins/Pdo/Facade.php
@@ -10,6 +10,32 @@ class Facade extends AbstractFacade
     /**
      * @param string $sql
      * @param array $params
+     * @return mixed|null
+     */
+    public function selectOne($sql, array $params = [])
+    {
+        $row = $this->selectOneRow($sql, $params, \PDO::FETCH_NUM);
+        return count($row) ? current($row) : null;
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     * @param int $fetchMode
+     * @return array|null
+     */
+    public function selectOneRow($sql, array $params = [], $fetchMode = \PDO::FETCH_ASSOC)
+    {
+        $pdo = $this->getPdo();
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($params);
+
+        return $stmt->fetch($fetchMode);
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
      * @param int $fetchMode
      * @return array
      */

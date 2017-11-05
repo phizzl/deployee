70eb8c680c30ad13a1906218fde26ca10238d1f0
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-11-05 08:53:47 +0100

    Added events

--- plugins/RunDeploy/Commands/DeployRunCommand.php
+++ plugins/RunDeploy/Commands/DeployRunCommand.php
@@ -10,8 +10,10 @@ use Deployee\Plugins\RunDeploy\Dispatcher\DispatcherFinder;
 use Deployee\Plugins\RunDeploy\Dispatcher\DispatchResult;
 use Deployee\Plugins\RunDeploy\Dispatcher\DispatchResultInterface;
 use Deployee\Plugins\RunDeploy\Events\FindExecutableDefinitionsEvent;
+use Deployee\Plugins\RunDeploy\Events\PostDispatchDeploymentEvent;
 use Deployee\Plugins\RunDeploy\Events\PostDispatchTaskEvent;
 use Deployee\Plugins\RunDeploy\Events\PostRunDeploy;
+use Deployee\Plugins\RunDeploy\Events\PreDispatchDeploymentEvent;
 use Deployee\Plugins\RunDeploy\Events\PreDispatchTaskEvent;
 use Deployee\Plugins\RunDeploy\Events\PreRunDeploy;
 use Deployee\Plugins\RunDeploy\Module;
@@ -43,11 +45,15 @@ class DeployRunCommand extends Command
         foreach($definitions as $className){
             $output->writeln(sprintf("Execute definition %s", $className), OutputInterface::VERBOSITY_VERBOSE);
             $deployment = $this->locator->Deployment()->getFactory()->createDeploymentDefinition($className);
+
+            $this->locator->Events()->dispatchEvent(PostDispatchTaskEvent::class, new PreDispatchDeploymentEvent($deployment));
             if($this->runDeploymentDefinition($deployment, $output) === true) {
                 $output->writeln(sprintf("Finished executing definition %s", $className), OutputInterface::VERBOSITY_DEBUG);
+                $this->locator->Events()->dispatchEvent(PostDispatchTaskEvent::class, new PostDispatchDeploymentEvent($deployment, true));
             }
             else{
                 $output->writeln(sprintf("Error while executing definition %s", $className));
+                $this->locator->Events()->dispatchEvent(PostDispatchTaskEvent::class, new PostDispatchDeploymentEvent($deployment, false));
                 $success = false;
                 break;
             }

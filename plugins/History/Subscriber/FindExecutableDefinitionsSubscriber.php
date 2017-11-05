4850cd9a8b9a8c208be800816e8b6ed2d59323e8
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-11-05 08:40:18 +0100

    Added subscriber

---
+++ plugins/History/Subscriber/FindExecutableDefinitionsSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Deployee\Plugins\History\Subscriber;
+
+
+use Deployee\Deployment\Definitions\Deployments\DeploymentDefinitionInterface;
+use Deployee\Kernel\Locator;
+use Deployee\Plugins\Pdo\Facade;
+use Deployee\Plugins\RunDeploy\Events\FindExecutableDefinitionsEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class FindExecutableDefinitionsSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var Locator
+     */
+    private $locator;
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            FindExecutableDefinitionsEvent::class => 'onFindExecutableDefinitions'
+        ];
+    }
+
+    /**
+     * FindExecutableDefinitionsSubscriber constructor.
+     * @param Locator $locator
+     */
+    public function __construct(Locator $locator)
+    {
+        $this->locator = $locator;
+    }
+
+    /**
+     * @param FindExecutableDefinitionsEvent $event
+     */
+    public function onFindExecutableDefinitions(FindExecutableDefinitionsEvent $event)
+    {
+        /* @var Facade $pdo */
+        $pdo = $this->locator->Pdo()->getFacade();
+        $executableDefinitions = [];
+        foreach($event->getDefinitions() as $className){
+            $sql = "SELECT COUNT(*) AS c FROM deployee_history_deployments WHERE name=:name AND exitcode=0";
+            if((int)$pdo->selectOne($sql, [':name' => $className]) === 0){
+                $executableDefinitions[] = $className;
+            }
+        }
+
+        $event->setDefinitions($executableDefinitions);
+    }
+}

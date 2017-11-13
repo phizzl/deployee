0e2f7ba760f08ae1c5bcc7732d9a3367c8c77d54
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-11-13 15:30:43 +0100

    Added definition for translating

--- plugins/i18n/Facade.php
+++ plugins/i18n/Facade.php
@@ -7,11 +7,35 @@ use Deployee\Kernel\Modules\AbstractFacade;
 
 class Facade extends AbstractFacade
 {
-    private $translations;
-
-    public function translate($ident, array $params = [])
+    /**
+     * @param string $lang
+     * @return TranslationCollection
+     */
+    private function getTranslations($lang)
     {
+        $translations = $this->locator->Dependency()->getFacade()->getDependency(Module::TRANSLATION_COLLECTION_DEPENDENCY);
+        if(!isset($translations[$lang])){
+            $lang = Module::LANG_US_EN;
+        }
 
+        return $translations[$lang];
+    }
+
+    /**
+     * @param string $lang
+     * @param string $ident
+     * @param array $params
+     * @return mixed
+     */
+    public function translate($lang, $ident, array $params = [])
+    {
+        $str = $this->getTranslations($lang)->get($ident);
+        $replace = [];
+        foreach($params as $name => $value){
+            $replace["%{$name}%"] = $value;
+        }
+
+        return str_replace(array_keys($replace), $replace, $str);
     }
 
 }

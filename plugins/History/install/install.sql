88a8583ed1984dc1a72e14925b6410cbc363aa15
Phillip Schleicher <schleicher@nexus-netsoft.com>
2017-11-14 16:29:03 +0100

    Changed install for DB history

--- plugins/History/install/install.sql
+++ plugins/History/install/install.sql
@@ -7,15 +7,16 @@ CREATE TABLE IF NOT EXISTS `deployee_history` (
   ENGINE=InnoDB
 ;
 
-CREATE TABLE IF NOT EXISTS `deployee_history_deployments` (
+CREATE TABLE `deployee_history_deployments` (
   `name` VARCHAR(255) NOT NULL,
   `deploytime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `success` TINYINT(1) UNSIGNED NOT NULL,
-  PRIMARY KEY (`name`)
+  INDEX `name_success` (`name`, `success`)
 )
   COLLATE='utf8_general_ci'
   ENGINE=InnoDB
 ;
+
 
 # Update old installations
 REPLACE INTO `deployee_history_deployments` (`name`, `deploytime`, `success`) SELECT `name`, `deploytime`, 1 FROM `deployee_history`;
